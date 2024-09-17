50c2b7f715c7d0a617e62cf4b2ed12da3bb78eec
Izushi <shoeshinesamurai@gmail.com>
2024-09-17 12:13:03 -0700

    Exercise4 JS

---
+++ js/Exercise4.js
@@ -0,0 +1,9 @@
+const A = 12;
+const B = 10;
+const C = 15;
+
+if (A + B > C && A + C > B && B + C > A) {
+    console.log('Making a triangle is possible');
+} else {
+    console.log('Making a triangle is impossible');
+}
