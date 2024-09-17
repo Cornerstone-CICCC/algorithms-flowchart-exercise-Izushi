f500083b000803a452a6d862ed47752ac4cac104
Izushi <shoeshinesamurai@gmail.com>
2024-09-17 12:13:51 -0700

    Exercise5 JS

---
+++ js/Exercise5.js
@@ -0,0 +1,12 @@
+let S = 80;
+const F = 100;
+
+if (S >= F * 0.8) {
+    console.log("grade A");
+} else if (S >= F * 0.6) {
+    console.log("grade B");
+} else if (S >= F * 0.4) {
+    console.log("grade C");
+} else {
+    console.log("No Grade");
+}
